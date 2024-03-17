e8d8d431f3a9382717aebc0ef32fb4203593142f
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-03-17 15:15:36 -0300

    FIX: width and border radius of mobile devices

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,7 +8,7 @@ const Header = () => {      // Header component
 
     // JSX
     return (
-        <header className="w-screen h-60 lg:w-7/12 lg:h-72 mx-auto rounded-b-lg shadow-xl bg-cover flex flex-col justify-center items-center gap-3" style={{ backgroundImage:"url(/assets/background.jpg)" }}>
+        <header className="w-screen h-60 xl:w-7/12 lg:h-72 mx-auto xl:rounded-b-lg shadow-xl bg-cover flex flex-col justify-center items-center gap-3" style={{ backgroundImage:"url(/assets/background.jpg)" }}>
                 <div>
                     <h1 className="text-white text-2xl lg:text-3xl font-bold text-center">{t("Title")}</h1>
                     <p className="text-white text-lg font-medium">{t("Introduction")}</p>
