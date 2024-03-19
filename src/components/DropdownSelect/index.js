13e535364a54ccd6a77456257fb2aa9c6a4baf06
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-03-18 21:43:14 -0300

    ADD: Dropdown component

---
+++ src/components/DropdownSelect/index.js
@@ -0,0 +1,29 @@
+import { useTranslation } from "react-i18next";     // i18next
+
+const DropdownSelect = ({ stock, toChange }) => {  // DropdownSelect component  
+    const { t } = useTranslation();     // Translation hook
+
+    // JSX
+    return (
+        <div className="space-y-2">
+            <label 
+                className="font-medium"
+                htmlFor="product"
+            >{t("Product")}</label>
+            <select 
+                className="input"
+                id="product"
+                name="product"
+                required={true}
+                onChange={e => toChange(e.target.value)}
+            >
+                <option value="">{t("Choose a product")}</option>
+                {stock.map(product => (
+                    <option key={product.id} value={product.name}>{t(product.name)}</option>
+                ))}
+            </select>
+        </div>
+    )
+};
+
+export default DropdownSelect;
