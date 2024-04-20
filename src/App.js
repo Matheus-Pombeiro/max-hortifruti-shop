0d75fac5e9aacb8fcc04b96b3147c231a190a774
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-04-20 15:53:26 -0300

    ADD: Footer component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 // Component imports
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
 const App = () => {   // App component
   // JSX
   return (
-    <div className="App min-h-screen w-dvw bg-green-100 dark:bg-zinc-500 flex flex-col items-center gap-5">
+    <div className="App min-h-screen w-screen bg-green-100 dark:bg-zinc-500 flex flex-col justify-between items-center gap-5">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import { useTranslation } from "react-i18next";     // i18next
+
+const Footer = () => {
+    const { t } = useTranslation();     // Translation hook
+
+    const date = new Date;
+    const year = date.getFullYear();
+
+    return (
+        <footer className="w-screen h-12 bg-green-50 flex justify-center items-center dark:bg-zinc-400">
+            <p className="text-lg lg:text-xl font-medium"><a href="https://www.linkedin.com/in/matheus-pombeiro/" target="_blank" rel="noreferrer noopener" className="hover:underline">Matheus Pombeiro</a> | {year}</p>
+        </footer>
+    )
+};
+
+export default Footer;
