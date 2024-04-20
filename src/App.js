a56fa3804d5c91af9267da60c1113be7b1000e33
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-04-20 15:17:56 -0300

    FIX: style

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Main from "./components/Main";
 const App = () => {   // App component
   // JSX
   return (
-    <div className="App bg-green-100 dark:bg-zinc-500 flex flex-col justify-between items-center gap-5">
+    <div className="App min-h-screen w-dvw bg-green-100 dark:bg-zinc-500 flex flex-col items-center gap-5">
       <Header />
       <Main />
     </div>
