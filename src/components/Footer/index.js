33939206df08f552049d7ed4176741e5c81b9b82
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-04-21 15:10:47 -0300

    FIX: dark mode background

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,8 +7,8 @@ const Footer = () => {
     const year = date.getFullYear();
 
     return (
-        <footer className="w-full h-12 bg-green-50 flex justify-center items-center dark:bg-zinc-400">
-            <p className="text-lg lg:text-xl font-medium"><a href="https://www.linkedin.com/in/matheus-pombeiro/" target="_blank" rel="noreferrer noopener" className="hover:underline">Matheus Pombeiro</a> | {year}</p>
+        <footer className="w-full h-12 bg-green-50 flex justify-center items-center dark:bg-stone-500">
+            <p className="text-lg lg:text-xl font-medium dark:text-white"><a href="https://www.linkedin.com/in/matheus-pombeiro/" target="_blank" rel="noreferrer noopener" className="hover:underline">Matheus Pombeiro</a> | {year}</p>
         </footer>
     )
 };
